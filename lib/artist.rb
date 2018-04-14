7031e1f80730e36b9d56a61cd9ce73bc39338c05
Shea Kusiak <shkusiak@gmail.com>
2018-04-14 19:14:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,16 @@
 class Artist
   attr_accessor :name
-
+  @@song_count = 0
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count = 0
+    #@@song_count = 0
   end
 
   def add_song(song)
-    @@song_count = @@song_count + 1
     @songs << song
-    song.artist = self #the songs artist is Artist
+    song.artist = self
+    @@song_count = @@song_count + 1
   end
 
   def songs
@@ -18,18 +18,13 @@ class Artist
   end
 
   def add_song_by_name(name)
-    @@song_count = @@song_count + 1
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@song_count = @@song_count + 1
   end
 
   def self.song_count
     @@song_count
   end
-#  def self.song_count
-#    array = @songs
-#    array = array.size
-#    array#@@song_count = @songs
-#  end
 end
